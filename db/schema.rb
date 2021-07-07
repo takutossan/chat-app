daab170067bdf7ce511d00280dd3f9b57d09f8ab
takutossan <gikushaku.4989@gmail.com>
2021-07-07 16:59:26 +0900

    メッセージの表示

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,13 @@ ActiveRecord::Schema.define(version: 2021_07_07_050328) do
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -57,6 +62,8 @@ ActiveRecord::Schema.define(version: 2021_07_07_050328) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
 end
